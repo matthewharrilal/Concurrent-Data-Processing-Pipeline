47628544a79ffa540df1228d5772f3d04ee6f66e
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-09-11 18:01:01 -0400

    Refactored out download service implementation out of network service and into it's own DownloadService

--- Concurrency-Data-Processing-Pipeline/Services/DownloadService.swift
+++ Concurrency-Data-Processing-Pipeline/Services/DownloadService.swift
@@ -6,8 +6,142 @@
 //
 
 import Foundation
+import UIKit
 
-class DownloadService {
+protocol DownloadProtocol {
+    func executeDownloadTask(url: URL, priority: Operation.QueuePriority, jobNumber: Int) async
+}
+
+class DownloadService: DownloadProtocol {
     
+    private let networkService: NetworkProtocol
+    private let maxConcurrentDownloadOperations: Int
+    private let maxConcurrentTransformationOperations: Int
+    private let maxConcurrentSaveOperations: Int
     
+    private var pendingDownloadOperations: [DownloadOperation] = []
+    
+    private var counter: Int = 0
+    private lazy var counterQueue = DispatchQueue(label: "serial.counterQueue")
+    
+    public var onFinishedDownload: ((Int, UIImage?) -> Void)?
+    
+    private lazy var downloadOperationQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = maxConcurrentDownloadOperations
+        queue.name = "Download Operation Queue"
+        return queue
+    }()
+    
+    init(
+        networkService: NetworkProtocol,
+        maxConcurrentDownloadOperations: Int = 2,
+        maxConcurrentTransformationOperations: Int = 2,
+        maxConcurrentSaveOperations: Int = 1
+    ) {
+        self.networkService = networkService
+        self.maxConcurrentDownloadOperations = maxConcurrentDownloadOperations
+        self.maxConcurrentTransformationOperations = maxConcurrentTransformationOperations
+        self.maxConcurrentSaveOperations = maxConcurrentSaveOperations
+    }
+    
+    func executeDownloadTask(url: URL, priority: Operation.QueuePriority, jobNumber: Int) async {
+        counterQueue.sync { [weak self] in
+            guard let self = self else { return }
+            self.counter += 1
+            
+            guard self.counter <= maxConcurrentDownloadOperations else {
+                self.counter -= 1
+                
+                print("")
+                print("Couldn't immedieately execute Job #\(jobNumber), will execute it after a task completes")
+                print("")
+                
+                let pendingOperation = DownloadOperation(
+                    operationURL: url,
+                    networkService: networkService,
+                    jobNumber: jobNumber
+                )
+                
+                pendingOperation.queuePriority = priority
+                
+                self.pendingDownloadOperations.append(
+                    pendingOperation
+                )
+                
+                return
+            }
+            
+            let downloadOperation = DownloadOperation(
+                operationURL: url,
+                networkService: networkService,
+                jobNumber: jobNumber
+            )
+            downloadOperation.queuePriority = priority
+            startDownloadTask(downloadOperation)
+        }
+    }
+}
+
+private extension DownloadService {
+    
+    func startDownloadTask(_ operation: DownloadOperation) {
+        guard !operation.isFinished, !operation.isExecuting else {
+            print("Operation for job #\(operation.jobNumber) is already finished or executing. Skipping...")
+            return
+        }
+        
+        print("Starting download task for job #\(operation.jobNumber)")
+        downloadOperationQueue.addOperation(operation)
+        
+        operation.onFinished = { [weak self] (jobNumber: Int, image: UIImage?) in
+            guard let self = self else {
+                print("Self has been deallocated")
+                return
+            }
+            
+            self.onFinishedDownload?(jobNumber, image)
+            print("Download operation complete for job #\(jobNumber)")
+            print("")
+            
+            self.counterQueue.sync {
+                self.counter -= 1 // Decrement counter of current operation since operation has been completed
+            }
+            
+            self.handlePendingDownloadTasks()
+        }
+    }
+    
+    /*
+     Nuanced Note:
+     - The reason we don't remove the pendingOperation from the array and then execute it is b/c pendingDownloadOperations is the only object bridiging and retaining the pendingOperations inside of it to self. If removed, we would lose access to self and the onFinished completion handler would not be invoked here as it would't know where to send the results upon invocation i.e here.
+     */
+    private func handlePendingDownloadTasks() {
+        var nextPendingOperation: DownloadOperation?
+        
+        counterQueue.sync {
+            guard
+                pendingDownloadOperations.count > 0,
+                let pendingOperation = pendingDownloadOperations.first
+            else {
+                print("")
+                print("No pending download tasks to handle.")
+                return
+            }
+            
+            print("")
+            print("Re-trying previously pending task job #\(pendingOperation.jobNumber)")
+            print("")
+            
+            // Need to access the first operation in pending operations before we remove so that we don't lose reference to self when invoking onFinished
+            nextPendingOperation = pendingOperation
+            pendingDownloadOperations.removeFirst()
+            
+            counter += 1
+        }
+        
+        if let nextPendingOperation = nextPendingOperation {
+            startDownloadTask(nextPendingOperation)
+        }
+    }
 }
